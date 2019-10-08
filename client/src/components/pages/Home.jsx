d8a206ef4803bae5ba42c57dafd3e2cd01fe2372
Cole Mooney <colemooney@Coles-MacBook-Pro.local>
2019-10-08 10:59:35 -0400

    test

---
+++ client/src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <h2>Home</h2>
+        <p>This is a sample project with the MERN stack</p>
+      </div>
+    )
+  }
+}
