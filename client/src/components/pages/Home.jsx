1412a6d6a566daaec9272b23065f0750ba1ca3c1
Christian Roque <croqu008@fiu.edu>
2019-10-10 09:14:16 -0400

    all Cs

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,71 +1,99 @@
 import React, { Component } from 'react';
-
 export default class Home extends Component {
 	render() {
 		return (
-			<div className="row">
-				<div className="col-md-3 ">
-					{/* USER PROFILE CARD */}
-
-					<div className="card">
-						<div class="avatar-flip">
-							<img
-								src="http://media.idownloadblog.com/wp-content/uploads/2012/04/Phil-Schiller-headshot-e1362692403868.jpg"
-								height="150"
-								width="150"
-							/>
-							<img
-								src="http://i1112.photobucket.com/albums/k497/animalsbeingdicks/abd-3-12-2015.gif~original"
-								height="250"
-								width="250"
-							/>
-						</div>
-						<div className="card-body">
-							<h5 className="card-title">User Name</h5>
-							<p className="card-text">User preferred address.</p>
-						</div>
-						<div className="card-body">
-							<a href="#" className="card-link">
-								Pet Settings
-							</a>
-							<br />
-							<a href="#" className="card-link">
-								User Settings
-							</a>
-						</div>
-					</div>
-				</div>
-				<div className="col-md-6">
-					{/* GEO LIST + EVENTS */}
-					<div class="container">
-						<div class="row">
-							<h2>Groomers near you!</h2>
-							<div class="col">
-								<ul>
-									<li>Username , Distance , Rate</li>
-									<li>Christian , 10mi , 40$/hr</li>
-									<li>Jose , 7mi , 50$/hr</li>
-									<li>Santa , 2000mi , 1,000$/hr</li>
-								</ul>
+			<div className="Home">
+				<div className="row">
+					<div className="col-md-3 ">
+						{/* USER PROFILE CARD */}
+						<div className="card">
+							<div className="avatar-flip">
+								<img
+									src="http://media.idownloadblog.com/wp-content/uploads/2012/04/Phil-Schiller-headshot-e1362692403868.jpg"
+									alt=""
+									height="150"
+									width="150"
+								/>
+								<img
+									src="https://s3-media1.fl.yelpcdn.com/bphoto/8brydl3V-sxA3i58-xx9tw/o.jpg"
+									alt=""
+									height="250"
+									width="250"
+								/>
+							</div>
+							<div className="card-body">
+								<h5 className="card-title">User Name</h5>
+								<p className="card-text">User preferred address.</p>
+							</div>
+							<div className="card-body">
+								<a href="/" className="card-link">
+									Pet Settings
+								</a>
+								<br />
+								<a href="/" className="card-link">
+									User Settings
+								</a>
 							</div>
 						</div>
 					</div>
-					<div class="container align-self-start">
-						<div class="row ">
-							<h2>Events in Miami</h2>
-							<div class="col">
-								<ul>
-									<li>Username , Content , Link</li>
-									<li>Christian , Dog park party at 7pm at hyde park , www.aspca.com</li>
-									<li>Jose , Free grooming at petsmart monday the 17th all day , www.petsmart.com</li>
-									<li>Santa , Dog day care special rate next week 50% off , www.doggydaycare.com</li>
-								</ul>
+					<div className="col-md-6">
+						{/* GEO LIST + EVENTS */}
+						{/* <div className="container"> */}
+						<div>
+							<div className="row">
+								<h2>Groomers near you!</h2>
+								<div className="col">
+									<ul>
+										<li>
+											Username , Distance , Rate
+											<hr />
+										</li>
+										<li>
+											Christian , 10mi , 40$/hr
+											<hr />
+										</li>
+										<li>
+											Jose , 7mi , 50$/hr
+											<hr />
+										</li>
+										<li>
+											Santa , 2000mi , 1,000$/hr
+											<hr />
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						{/* <div className="container"> */}
+						<div>
+							<div className="row ">
+								<h2>Events in Miami</h2>
+								<div className="col">
+									<ul>
+										<li>
+											Username , Content , Link
+											<hr />
+										</li>
+										<li>
+											Christian , Dog park party at 7pm at hyde park , www.aspca.com
+											<hr />
+										</li>
+										<li>
+											Jose , Free grooming at petsmart monday the 17th all day , www.petsmart.com
+											<hr />
+										</li>
+										<li>
+											Santa , Dog day care special rate next week 50% off , www.doggydaycare.com
+											<hr />
+										</li>
+									</ul>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div className="col-md-3">
-					<h1>TRENDING NEWS</h1>
+					<div className="col-md-3">
+						<h1>TRENDING NEWS</h1>
+					</div>
 				</div>
 			</div>
 		);
