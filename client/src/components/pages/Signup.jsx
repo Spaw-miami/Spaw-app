b319c3888011d7fd8311e681c9fa0c2d2729b286
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-10-18 09:44:49 -0400

    heroku

--- client/src/components/pages/Signup.jsx
+++ client/src/components/pages/Signup.jsx
@@ -55,7 +55,7 @@ export default class Signup extends Component {
       address: this.state.address
     };
     axios
-      .post('http://spaws-project.herokuapp.com//create/user', data)
+      .post('http://spaws-project.herokuapp.com/create/user', data)
       .then((user) => {
         console.log(user);
         this.props.setUser(user.data)
